7d63cc34930ed04b43128068b03c0b0ba3b6d3dc
samdyra <197413vr@gmail.com>
2022-02-11 21:35:27 +0700

    progress on navbar

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,5 +1,13 @@
 const NavBar = () => {
-  return <nav></nav>;
+  return (
+    <div>
+      <nav className="flex flex-row bg-slate-700">
+        <ul>Map Screen</ul>
+        <ul>DownloadMapScreen</ul>
+        <ul>About Screen</ul>
+      </nav>
+    </div>
+  );
 };
 
 export default NavBar;
