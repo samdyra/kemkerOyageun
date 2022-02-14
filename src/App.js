28036cc60c6c1765cd57a7deb9d863464596b409
samdyra <197413vr@gmail.com>
2022-02-14 21:53:28 +0700

    add map to mapscreen

--- src/App.js
+++ src/App.js
@@ -1,7 +1,24 @@
 import MapScreen from "./screens/MapScreen";
-
+import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
+import "./App.css";
+import NavBar from "./components/NavBar";
 function App() {
-  return <MapScreen></MapScreen>;
+  return (
+    <div>
+      <NavBar></NavBar>
+      <MapContainer center={[51.505, -0.09]} zoom={13}>
+        <TileLayer
+          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        />
+        <Marker position={[51.505, -0.09]}>
+          <Popup>
+            A pretty CSS3 popup. <br /> Easily customizable.
+          </Popup>
+        </Marker>
+      </MapContainer>
+    </div>
+  );
 }
 
 export default App;
--- src/screens/MapScreen.js
+++ src/screens/MapScreen.js
@@ -1,10 +1,10 @@
+import map from "../components/Map";
 import NavBar from "../components/NavBar";
 
 const MapScreen = () => {
   return (
     <>
       <NavBar></NavBar>
-      <p>this is map screen</p>
     </>
   );
 };
