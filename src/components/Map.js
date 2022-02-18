6bc03c55c092bc2af1440bf42dc9faff878eb716
samdyra <197413vr@gmail.com>
2022-02-18 21:39:27 +0700

    make MapDetails

--- src/components/Map.js
+++ src/components/Map.js
@@ -3,11 +3,27 @@ import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
 const Map = () => {
   return (
     <>
-      <div style={{ height: "calc(100vh - 64px)" }}>
+      <div
+        style={{
+          height: "calc(100vh - 64px)",
+          display: "flex",
+          flexDirection: "row-reverse",
+        }}
+      >
+        <div
+          style={{
+            width: "750px",
+            flex: "0 0 750px",
+          }}
+        ></div>
         <MapContainer
           center={[51.505, -0.09]}
           zoom={13}
-          style={{ height: "100%" }}
+          style={{
+            height: "100%",
+            position: "relative",
+            flex: "1 1 100%",
+          }}
         >
           <TileLayer
             attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
---
+++ src/components/MapDetails.js
@@ -0,0 +1,5 @@
+const MapDetails = () => {
+  return <div></div>;
+};
+
+export default MapDetails;
