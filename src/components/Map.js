1ccc9de339bb618f666d635f73d89e85f6d51b3b
samdyra <197413vr@gmail.com>
2022-02-22 20:34:15 +0700

    progress on AboutScreen and MapDetails

--- src/components/Map.js
+++ src/components/Map.js
@@ -1,19 +1,30 @@
+import { useState } from "react";
 import { MapContainer, TileLayer, GeoJSON } from "react-leaflet";
 
 const Map = ({ styling, studyArea }) => {
+  const [state, setState] = useState("");
+  console.log(state);
+
+  const onEachPolygons = (feature, layer) => {
+    const borderWidth = feature.properties.NAME;
+    console.log(feature);
+    console.log(layer);
+    layer.bindPopup(borderWidth);
+  };
   return (
     <>
       <MapContainer
         center={[-6.664775841757377, 108.41674126660926]}
-        zoom={17}
+        zoom={16}
         style={styling}
       >
         <TileLayer
           attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
           url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
         />
-        <GeoJSON data={studyArea}></GeoJSON>
+        <GeoJSON data={studyArea} onEachFeature={onEachPolygons}></GeoJSON>
       </MapContainer>
+      <text>{state}</text>
     </>
   );
 };
--- src/screens/AboutScreen.js
+++ src/screens/AboutScreen.js
@@ -1,3 +1,5 @@
+import "../assets/css/main.css";
+
 const AboutScreen = () => {
   return <div></div>;
 };
