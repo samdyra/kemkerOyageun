3d5c4c870219c2bce3f779e59bb81cf0afd3fd7c
samdyra <197413vr@gmail.com>
2022-03-01 21:29:31 +0700

    Editing mapdetails. god i love doing this project so much

--- src/screens/MapScreen.js
+++ src/screens/MapScreen.js
@@ -1,16 +1,27 @@
-import data from "../shapefiles/itbCirebon.json";
+import data from "../shapefiles/ITBJatinagor.json";
 import { useState } from "react";
 import { MapContainer, TileLayer, GeoJSON } from "react-leaflet";
+import noPic from "../images/no-image.jpg";
 
 const MapScreen = () => {
   const [name, setName] = useState("");
-  const [layer, setLayer] = useState("");
+  const [firstImage, setFirstImage] = useState("https://gdurl.com/ra7En");
+  const [secondImage, setSecondImage] = useState("https://gdurl.com/ra7En");
+  const [thirdImage, setThridImage] = useState("https://gdurl.com/ra7En");
+  const defaultFontSize = "30px";
+  const secondaryFontSize = "20px";
+
   const onEachPolygons = (feature, layer) => {
-    const borderName = feature.properties.NAME;
-    const borderLayer = feature.properties.LAYER;
+    const polygonName = feature.properties.Jenis_Obj;
+    const firstImage = feature.properties.Foto_1;
+    const secondImage = feature.properties.Foto_2;
+    const thirdImage = feature.properties.Foto_2;
+
     function someFunc() {
-      setName(borderName);
-      setLayer(borderLayer);
+      setName(polygonName);
+      setFirstImage(firstImage);
+      setSecondImage(secondImage);
+      setThridImage(thirdImage);
     }
     layer.on({
       click: (event) => {
@@ -34,25 +45,84 @@ const MapScreen = () => {
       {/* Settings Bar */}
       <div className="flex flex-row-reverse h-[100%]">
         {/* Maps Detail */}
-        <div style={{ width: "600px", flex: "0 0 600px" }}>
-          <text>
+        <div
+          style={{
+            width: "600px",
+            flex: "0 0 600px",
+            boxShadow: "-2px 2px 5px 0 rgba(0,0,0,.4)",
+            zIndex: 3,
+            position: "relative",
+            overflowY: "scroll",
+            paddingLeft: "20px",
+            paddingTop: "10px",
+          }}
+        >
+          {/* Map Title */}
+          <div style={{ display: "flex", flexDirection: "column" }}>
+            <text
+              style={{
+                fontSize: defaultFontSize,
+                lineHeight: defaultFontSize * 1.3,
+                fontWeight: "600",
+              }}
+            >
+              Institut Teknologi Bandung
+            </text>
+            <text
+              style={{
+                fontSize: secondaryFontSize,
+                lineHeight: secondaryFontSize * 1.3,
+                fontWeight: "400",
+              }}
+            >
+              Kampus Cirebon
+            </text>
+          </div>
+          {/* Map Title */}
+          {/* Map Images */}
+          <div
+            style={{
+              paddingTop: "15px",
+              display: "flex",
+              flexDirection: "column",
+            }}
+          >
             <img
-              src="../components/hei.jpg"
-              style={{ height: "300px", width: "300px" }}
+              src={firstImage}
+              style={{ height: "400px", width: "540px", position: "relative" }}
             ></img>
-          </text>
+            <div style={{ display: "flex", flexDirection: "row" }}>
+              <img
+                src={secondImage}
+                style={{
+                  height: "270px",
+                  width: "270px",
+                  position: "relative",
+                }}
+              ></img>
+              <img
+                src={thirdImage}
+                style={{
+                  height: "270px",
+                  width: "270px",
+                  position: "relative",
+                }}
+              ></img>
+            </div>
+          </div>
         </div>
+        {/* Map Images */}
         {/* Maps Detail */}
         {/* Maps */}
         <MapContainer
-          center={[-6.664775841757377, 108.41674126660926]}
+          center={[-6.927348780149033, 107.7695420971685]}
           zoom={16}
           style={{
             height: "100%",
             position: "relative",
             flex: "1 1 100%",
             boxShadow: "-2px 3px 5px 0 rgba(0,.9,0,.4)",
-            zIndex: 1,
+            zIndex: 0,
           }}
         >
           <TileLayer
